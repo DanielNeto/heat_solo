23c9de6073c50dafac7bef3ac40ede05cb80e6ec
Daniel Neto <daniel.jsilvan@gmail.com>
2018-07-03 01:31:47 -0300

    Adding vlink, vhost and rest user/pw parameters

--- resources/sdn_overlay.py
+++ resources/sdn_overlay.py
@@ -72,27 +72,27 @@ class SDNOverlay(resource.Resource):
         DATAPATH: properties.Schema(
             properties.Schema.STRING,
             _('Openflow datapath ID of the virtual switch.'),
-            required=True,
+            required=True
         ),
         CONTROLLER_IP: properties.Schema(
             properties.Schema.STRING,
             _('Openflow controller IP address.'),
-            required=True,
+            required=True
         ),
         CONTROLLER_PORT: properties.Schema(
             properties.Schema.STRING,
             _('Openflow controller TCP port.'),
-            required=True,
+            required=True
         ),
         OPENFLOW_VERSION: properties.Schema(
             properties.Schema.STRING,
             _('Openflow version.'),
-            required=True,
+            required=True
         ),
         PHYSICAL_DEVICE: properties.Schema(
             properties.Schema.STRING,
             _('Physical device name.'),
-            required=True,
+            required=True
         )
     }
 
@@ -100,26 +100,120 @@ class SDNOverlay(resource.Resource):
         DATAPATH: properties.Schema(
             properties.Schema.STRING,
             _('Openflow datapath ID of the virtual switch.'),
-            required=True,
+            required=True
         ),
         VIRTUAL_PORT_NUMBER: properties.Schema(
             properties.Schema.STRING,
             _('Openflow virtual port number.'),
-            required=True,
+            required=True
         ),
         PHYSICAL_PORT_NAME: properties.Schema(
             properties.Schema.STRING,
             _('Physical port name.'),
-            required=True,
+            required=True
         ),
         BINDING_TYPE: properties.Schema(
             properties.Schema.STRING,
             _('Binding type between virtual and physical ports.'),
-            required=True,
+            required=True
         ),
         VLAN: properties.Schema(
             properties.Schema.STRING,
             _('VLAN identifier for VLAN binding type.')
+        )
+    }
+
+    _vlink_schema = {
+        DATAPATH_SRC: properties.Schema(
+            properties.Schema.STRING,
+            _('Openflow datapath ID of the source virtual switch.'),
+            required=True
+        ),
+        DATAPATH_DST: properties.Schema(
+            properties.Schema.STRING,
+            _('Openflow datapath ID of the destination virtual switch.'),
+            required=True
+        ),
+        VIRTUAL_PORT_NUMBER_SRC: properties.Schema(
+            properties.Schema.STRING,
+            _('Openflow source virtual port number.'),
+            required=True
+        ),
+        VIRTUAL_PORT_NUMBER_DST: properties.Schema(
+            properties.Schema.STRING,
+            _('Openflow destination virtual port number.'),
+            required=True
+        ),
+        PHYSICAL_PORT_NAME_SRC: properties.Schema(
+            properties.Schema.STRING,
+            _('Source physical port name.'),
+            required=True
+        ),
+        PHYSICAL_PORT_NAME_DST: properties.Schema(
+            properties.Schema.STRING,
+            _('Destination physical port name.'),
+            required=True
+        ),
+        LINK_TYPE: properties.Schema(
+            properties.Schema.STRING,
+            _('Link tunnel type.'),
+            required=True
+        ),
+        VLAN: properties.Schema(
+            properties.Schema.STRING,
+            _('VLAN identifier for STATIC_VLAN link type.')
+        ),
+        NSI_BANDWIDTH: properties.Schema(
+            properties.Schema.STRING,
+            _('Bandwidth value for NSI link type.')
+        )
+    }
+
+    _vhost_schema = {
+        HOSTNAME: properties.Schema(
+            properties.Schema.STRING,
+            _('Virtual host name.'),
+            required=True
+        ),
+        ENDPOINT_NAME: properties.Schema(
+            properties.Schema.STRING,
+            _('Endpoint type name.'),
+            required=True
+        ),
+        TEMPLATE_NAME: properties.Schema(
+            properties.Schema.STRING,
+            _('Template image name.'),
+            required=True
+        ),
+        DATAPATH: properties.Schema(
+            properties.Schema.STRING,
+            _('Openflow datapath ID of the virtual switch.'),
+            required=True
+        ),
+        VIRTUAL_PORT_NUMBER: properties.Schema(
+            properties.Schema.STRING,
+            _('Virtual port number to connect with the virtual switch.'),
+            required=True
+        ),
+        VLAN: properties.Schema(
+            properties.Schema.STRING,
+            _('VLAN identifier for binding the virtual host and switch.'),
+            required=True
+        ),
+        DATAPLANE_IP: properties.Schema(
+            properties.Schema.STRING,
+            _('Dataplane IP address.'),
+            required=True
+        ),
+        DATAPLANE_SUBNET: properties.Schema(
+            properties.Schema.STRING,
+            _('Dataplane subnet address.'),
+            required=True
+        ),
+        DATAPLANE_GATEWAY: properties.Schema(
+            properties.Schema.STRING,
+            _('Dataplane gateway address.'),
+            required=True
         )
     }
 
@@ -129,6 +223,18 @@ class SDNOverlay(resource.Resource):
             _('The REST API IP address and TCP port used to send requests '
               'to the SDNOverlay service.'
               'Ex.: http://172.25.0.2:8181')
+        ),
+        REST_USER: properties.Schema(
+            properties.Schema.STRING,
+            _('The REST API user used to send requests '
+              'to the SDNOverlay service.'
+              'Ex.: karaf')
+        ),
+        REST_PASSWORD: properties.Schema(
+            properties.Schema.STRING,
+            _('The REST API password used to send requests '
+              'to the SDNOverlay service.'
+              'Ex.: karaf')
         ),
         NETWORK_NAME: properties.Schema(
             properties.Schema.STRING,
@@ -140,7 +246,7 @@ class SDNOverlay(resource.Resource):
             _('List with 0 or more map elements containing virtual switch details.'),
             schema=properties.Schema(
                 properties.Schema.MAP,
-                schema=_vswitch_schema,
+                schema=_vswitch_schema
             ),
             update_allowed=True,
         ),
@@ -149,7 +255,25 @@ class SDNOverlay(resource.Resource):
             _('List with 0 or more map elements containing virtual port details.'),
             schema=properties.Schema(
                 properties.Schema.MAP,
-                schema=_vport_schema,
+                schema=_vport_schema
+            ),
+            update_allowed=True,
+        ),
+        LINKS: properties.Schema(
+            properties.Schema.LIST,
+            _('List with 0 or more map elements containing virtual link details.'),
+            schema=properties.Schema(
+                properties.Schema.MAP,
+                schema=_vlink_schema
+            ),
+            update_allowed=True,
+        ),
+        HOSTS: properties.Schema(
+            properties.Schema.LIST,
+            _('List with 0 or more map elements containing virtual host details.'),
+            schema=properties.Schema(
+                properties.Schema.MAP,
+                schema=_vhost_schema
             ),
             update_allowed=True,
         )
@@ -157,8 +281,8 @@ class SDNOverlay(resource.Resource):
 
     def getClient(self):
         rest_api = self.properties[self.REST_ADDRESS]
-        user = "karaf"
-        pw = "karaf"
+        user = self.properties[self.REST_USER]
+        pw = self.properties[self.REST_PASSWORD]
         return SoloClient(endpoint=rest_api, user=user, password=pw)
 
     def handle_create(self):
@@ -172,6 +296,12 @@ class SDNOverlay(resource.Resource):
 
         vports = self.properties[self.PORTS] or []
         self._addVPorts(vnets, vports)
+
+        vlinks = self.properties[self.LINKS] or []
+        self._addVLinks(vnets, vlinks)
+
+        vhosts = self.properties[self.HOSTS] or []
+        self._addVHosts(vnets, vhosts)
 
         networkName = client.createVNet(vnets.getJson())
 
@@ -224,6 +354,32 @@ class SDNOverlay(resource.Resource):
             binding = vport[self.BINDING_TYPE]
             vlan = vport[self.VLAN] or None
             vnets.addVPort(datapath, vport_number, pport_name, binding, vlan)
+
+    def _addVLinks(self, vnets, vlinks):
+        for vlink in vlinks:
+            dpid_src = vlink[self.DATAPATH_SRC]
+            dpid_dst = vlink[self.DATAPATH_DST]
+            vport_src = vlink[self.VIRTUAL_PORT_NUMBER_SRC]
+            vport_dst = vlink[self.VIRTUAL_PORT_NUMBER_DST]
+            pport_src = vlink[self.PHYSICAL_PORT_NAME_SRC]
+            pport_dst = vlink[self.PHYSICAL_PORT_NAME_DST]
+            link_type = vlink[self.LINK_TYPE]
+            vlan = vlink[self.VLAN] or None
+            nsi_bw = vlink[self.NSI_BANDWIDTH] or None
+            vnets.addVLink(dpid_src, dpid_dst, vport_src, vport_dst, pport_src, pport_dst, link_type, vlan, nsi_bw)
+
+    def _addVHosts(self, vnets, vhosts):
+        for vhost in vhosts:
+            hostname = vhost[self.HOSTNAME]
+            endpoint = vhost[self.ENDPOINT_NAME]
+            template = vhost[self.TEMPLATE_NAME]
+            datapath = vhost[self.DATAPATH]
+            vport = vhost[self.VIRTUAL_PORT_NUMBER]
+            vlan = vhost[self.VLAN]
+            dp_ip = vhost[self.DATAPLANE_IP]
+            dp_sub = vhost[self.DATAPLANE_SUBNET]
+            dp_gw = vhost[self.DATAPLANE_GATEWAY]
+            vnets.addVHost(hostname, endpoint, template, datapath, vport, vlan, dp_ip, dp_sub, dp_gw)
 
 def resource_mapping():
     return {
